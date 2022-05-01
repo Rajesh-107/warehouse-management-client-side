7b281f4f2a52ba3f440036ebddc5e9cb27ed1595
Rajesh-107 <applerb326@gmail.com>
2022-05-01 22:37:56 +0600

    added banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,8 +4,10 @@ import banner from '../../images/banner/jon-flobrant-lRSChvh1Mhs-unsplash.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <img src={banner} alt="" />
+        <div className='banner' style={{backgroundImage: `url(${banner})`}}>
+            <h1>World's Biggest Warehouse</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /> Deleniti molestiae labore a illo eius officia?</p>
+            {/* <img src={banner} alt="" /> */}
         </div>
     );
 };
